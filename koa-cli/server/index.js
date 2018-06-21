1e3fb05568d70bcfc16134a9070a3060a9c3762c
li305263 <164436886@qq.com>
2018-06-21 17:33:45 +0800

    update

--- koa-cli/server/index.js
+++ koa-cli/server/index.js
@@ -1,5 +1,40 @@
- const koa = require('koa')
- const app = new koa()
- app.use(async function(cxt,next) {
-     cxt.body = 'hello koa2'
- }).listen(3001)
+const koa = require('koa')
+const app = new koa()
+const router = require('./router/index')
+const koaBody = require('koa-body')
+
+ app.use(async (ctx,next)=>{
+    try{
+        await next()
+    }catch (err){
+        ctx.status = err.statusCode || err.status || 500
+        cts.bosy = {
+            msg:err.message
+        }
+    }
+ })
+ // x-response-time
+app.use(async (ctx,next)=>{
+    const start = Date.now()
+
+    await next()
+
+    const ms = Date.now() - start
+    ctx.set('X-Response-Time', `${ms}ms`)
+})
+//logger
+app.use(async(ctx,next)=>{
+    const start = Date.now()
+
+    await next()
+
+    const ms = Date.now() - start
+
+    console.log(`${ctx.method} ${ctx.url} ${ms}ms`)
+})
+
+app.use(koaBody())
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(3001)
