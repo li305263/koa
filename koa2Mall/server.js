12478cde792450c17250eb6ad2d365cfccd8a91b
li305263 <164436886@qq.com>
2018-06-29 21:09:14 +0800

    更新

---
+++ koa2Mall/server.js
@@ -0,0 +1,45 @@
+const koa = require('koa')
+const app = new koa()
+const router = require('./server/router')
+const koaBody = require('koa-body')
+const connect = require('./server/database')
+
+;(async ()=>{
+ await connect
+})()
+
+ app.use(async (ctx,next)=>{
+    try{
+        await next()
+    }catch (err){
+        ctx.status = err.statusCode || err.status || 500
+        cts.bosy = {
+            msg:err.message
+        }
+    }
+ })
+ // x-response-time
+app.use(async (ctx,next)=>{
+    const start = Date.now()
+
+    await next()
+
+    const ms = Date.now() - start
+    ctx.set('X-Response-Time', `${ms}ms`)
+})
+//logger
+app.use(async(ctx,next)=>{
+    const start = Date.now()
+
+    await next()
+
+    const ms = Date.now() - start
+
+    console.log(`${ctx.method} ${ctx.url} ${ms}ms`)
+})
+
+app.use(koaBody())
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(3001)
