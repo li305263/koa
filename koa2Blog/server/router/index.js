19907ad422c22f99e4b0793a5a4599599130dc4f
li305263 <164436886@qq.com>
2018-07-03 16:39:46 +0800

    测试

--- koa2Blog/server/router/index.js
+++ koa2Blog/server/router/index.js
@@ -3,13 +3,16 @@ const mongoose = require('mongoose')
 const User = require('../database/schema/user')
 const router = new Router()
 
-router.get('/',async (ctx,next)=>{
+router.post('/user_list',async (ctx,next)=>{
+
+
     let user = mongoose.model('User')
     let data = await user.find()
     ctx.body = {
         code:0,
         data
     }
+
 })
 
 module.exports = router
